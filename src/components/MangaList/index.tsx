4049e4562f7a6e6df956d434587f4282e2a5b6cb
Good Mood <09010581923017@student.unsri.ac.id>
2024-01-13 02:36:14 +0700

    Fixed Stuff

--- src/components/MangaList/index.tsx
+++ src/components/MangaList/index.tsx
@@ -5,22 +5,22 @@ export default function MangaList({ api }) {
   return (
     <>
       <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4">
-        {api?.map((mangaa: any) => (
+        {api?.map((manga: any) => (
           <Link
-            href={`/manga/${mangaa.mal_id}`}
-            key={mangaa.mal_id}
+            href={`/manga/${manga.mal_id}`}
+            key={manga.mal_id}
             className="bg-secondary dark:bg-dark-secondary shadow-slate-600 shadow-lg cursor-pointer"
           >
             <Image
-              src={mangaa.images.webp.image_url}
-              alt={mangaa.title}
+              src={manga.images.webp.image_url}
+              alt={manga.title}
               width={350}
               height={350}
               className="w-full max-h-64 md:max-h-80 object-cover"
             />
 
             <h3 className="text-third dark:text-dark-contrast text-center font-semibold p-2">
-              {mangaa.title}
+              {manga.title}
             </h3>
           </Link>
         ))}
